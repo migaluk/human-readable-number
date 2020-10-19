3514ed8a26a68aa932ac3da83f8f43f92d178446
Tanya <tanyamigaluk@gmail.com>
2020-10-20 01:01:26 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
+  if (number === 0) return 'zero';
+  let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let hundreds = 'hundred';
+  let result = '';
+  if (number > 99) {result += `${units[Math.floor(number / 100)]} ${hundreds}`};
+    if (number % 100 > 19) {
+        result += ` ${dozens[Math.floor(number % 100 / 10)]}`
+        if(number % 10 !== 0) {
+            result += ` ${units[Math.floor(number % 10)]}`;
+        }
+    }
+    if (number % 100 <= 19 && number % 100 > 0) result += ` ${units[Math.floor(number % 100)]}`;
+
+return result.trim();
+
 }
